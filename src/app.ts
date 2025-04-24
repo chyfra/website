18c94014bdb6ef6f11144a2d9f1ff707a84cb250
Tarek Jellali <jellalitarek@gmail.com>
2025-04-24 02:01:09 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,3 @@
 import './styles.scss';
-import Data from './assets/data.json';
 
-export class App {
-    private message: string;
-
-    constructor(msg: string) {
-      this.message = msg;
-    }
-
-    public printHello(id: string) {
-      const container = document.getElementById(id);
-      if (!!container) {
-        container.innerHTML = this.message;
-        const a = 'aaaa';
-      } else {
-        console.error(`<element id="${id}" ...> does not exist !`);
-      }
-    }
-}
-
-const appInstance = new App('Hello Webpack and TypeScript ♥');
-appInstance.printHello('app');
-console.log(Data);
+console.log("Hello curious one! It's coming soon :) ");
